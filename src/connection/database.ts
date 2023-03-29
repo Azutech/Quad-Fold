696122c2c6c5e9ee64ecea1a8385db85cdbef575
Emmanuel Onugha <azunna.onugha@gmail.com>
2023-03-29 13:08:01 +0100

    docker-compose added

---
+++ src/connection/database.ts
@@ -0,0 +1,16 @@
+import { Sequelize, DataTypes } from "sequelize";
+require("dotenv").config();
+
+const POSTGRES_URL = process.env.DATABASE_URL as unknown as string;
+const sequelize = new Sequelize(POSTGRES_URL);
+
+async function connectDB() {
+  try {
+    await sequelize.authenticate();
+    console.log("✅ Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+}
+
+export { connectDB, sequelize, Sequelize, DataTypes };
