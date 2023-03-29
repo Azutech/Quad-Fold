001eb7cd38243e29b48fdf1e284139792d6a5288
Emmanuel Onugha <azunna.onugha@gmail.com>
2023-03-29 15:21:43 +0100

    prod url added

--- dist/connection/database.js
+++ dist/connection/database.js
@@ -1,68 +1,35 @@
 "use strict";
-var __awaiter =
-  (this && this.__awaiter) ||
-  function (thisArg, _arguments, P, generator) {
-    function adopt(value) {
-      return value instanceof P
-        ? value
-        : new P(function (resolve) {
-            resolve(value);
-          });
-    }
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
-      function fulfilled(value) {
-        try {
-          step(generator.next(value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function rejected(value) {
-        try {
-          step(generator["throw"](value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function step(result) {
-        result.done
-          ? resolve(result.value)
-          : adopt(result.value).then(fulfilled, rejected);
-      }
-      step((generator = generator.apply(thisArg, _arguments || [])).next());
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-  };
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.DataTypes =
-  exports.Sequelize =
-  exports.sequelize =
-  exports.connectDB =
-    void 0;
+exports.DataTypes = exports.Sequelize = exports.sequelize = exports.connectDB = void 0;
 const sequelize_1 = require("sequelize");
-Object.defineProperty(exports, "Sequelize", {
-  enumerable: true,
-  get: function () {
-    return sequelize_1.Sequelize;
-  },
-});
-Object.defineProperty(exports, "DataTypes", {
-  enumerable: true,
-  get: function () {
-    return sequelize_1.DataTypes;
-  },
-});
-require("dotenv").config();
+Object.defineProperty(exports, "Sequelize", { enumerable: true, get: function () { return sequelize_1.Sequelize; } });
+Object.defineProperty(exports, "DataTypes", { enumerable: true, get: function () { return sequelize_1.DataTypes; } });
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const POSTGRES_URL = process.env.DATABASE_URL;
 const sequelize = new sequelize_1.Sequelize(POSTGRES_URL);
 exports.sequelize = sequelize;
 function connectDB() {
-  return __awaiter(this, void 0, void 0, function* () {
-    try {
-      yield sequelize.authenticate();
-      console.log("✅ Connection has been established successfully.");
-    } catch (error) {
-      console.error("Unable to connect to the database:", error);
-    }
-  });
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            yield sequelize.authenticate();
+            console.log("✅ Connection has been established successfully.");
+        }
+        catch (error) {
+            console.error("Unable to connect to the database:", error);
+        }
+    });
 }
 exports.connectDB = connectDB;
