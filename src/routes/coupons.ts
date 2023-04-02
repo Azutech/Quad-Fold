1ef628996b1118efd5e0bef99798ea7db0170e4a
Emmanuel Onugha <azunna.onugha@gmail.com>
2023-04-02 19:31:02 +0100

    coupons discounts added

--- src/routes/coupons.ts
+++ src/routes/coupons.ts
@@ -1,6 +1,10 @@
 import { Router } from "express";
-import { createCoupons } from "../controllers/coupons";
+import { createCoupons, discount, findAllCoupons, getOneCoupon, destroyCoupon } from "../controllers/coupons";
 
 export const coupons = Router()
 
 coupons.post('/createCoupons', createCoupons)
+coupons.post('/coupons/discount', discount)
+coupons.get('/getAllCoupons', findAllCoupons)
+coupons.get('/getOneCoupons/:id', getOneCoupon)
+coupons.get('/destroyCoupons/:id', destroyCoupon)
--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express'
-import { createProduct } from '../controllers/products'
+import { createProduct ,getOneProduct, findAllProducts, destroyProduct} from '../controllers/products'
 
 export const product = Router()
 
 product.post('/addProducts', createProduct)
+product.get('/addProducts/:id', getOneProduct)
+product.get('/addProducts', findAllProducts)
+product.delete('/addProducts/:id', destroyProduct)
